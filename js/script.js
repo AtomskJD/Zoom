cdae344495aa59462a4e8982e092f7c6cefe7a2a
AtomskJD <AtomskJD@gmail.com>
2013-05-27 23:35:59 +0600

    start

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+$('#container').isotope({
+  // options
+  itemSelector : '.item',
+  layoutMode : 'fitRows'
+});
+
+
+$('.row').isotope({
+  // options
+  itemSelector : '.color1',
+  layoutMode : 'fitRows'
+});
+$(".row").first().height($(window).height() / 2);	
+
+$('h1').click(function(){
+	$(".row").first().height($(document).height() +200);	
+});
+$('h2').click(function(){
+	$(document).scrollTo('#scroll', 800);	
+});
+});
