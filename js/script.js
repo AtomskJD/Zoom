5c51177dc580cb568d095f972206bba058000d50
Atomsk <atomskjd@gmail.com>
2013-05-31 19:39:54 +0600

    halfsize function
    функция изменения высоты начального экрана для десктопов

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
-var zoom = {
+var Zoom = {
+	mobile: 480,
+	tablet: 768,
+	desktop: 980,
 
+	init: function(container){
+		var _this = this;
+		/*init полуразмер банера*/
+		if(container.find('.half-sized').length){
+			Zoom.HalfSize.init(container.find('.half-sized'));
+
+			$(window).resize(function(){
+				Zoom.HalfSize.init(container.find('.half-sized'));
+				});
+		}
+		
+	}
+
+};
+
+Zoom.HalfSize = {
+	init: function(e){
+		$(e).each(function(){
+			if($(window).width()>Zoom.desktop){
+				$(this).height($(window).height() / 2);
+			}else if( ($(window).width()<Zoom.desktop) && ($(window).width()>=Zoom.tablet) ) {
+				//$(this).height($(window).height() / 4);
+				//console.log("есть свойство")
+				if($(this).attr('style')){
+					$(this).removeAttr('style');
+				}
+				//$(this).attr
+			}
+		});
+
+	}
 }
+
+$(function(){
+	Zoom.init($('body'));
+	$(window).scrollTo(0,400);
+});
 
 
 /*$(function(){
